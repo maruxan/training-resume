c3b0328cdaeaa22500482e49293270f499c3b626
Mauricio Garavaglia <garavagliamauricio@gmail.com>
2021-11-16 09:47:43 -0300

    chore(projects): created projects data file

---
+++ src/data/projects.js
@@ -0,0 +1,81 @@
+export const htmlCss = [
+  {
+    name: 'article-1',
+    url: 'https://maruxan.github.io/html-css-practice/article-1/',
+  },
+  {
+    name: 'article-2',
+    url: 'https://maruxan.github.io/html-css-practice/article-2/',
+  },
+  {
+    name: 'card-1',
+    url: 'https://maruxan.github.io/html-css-practice/card-1/',
+  },
+  {
+    name: 'card-2',
+    url: 'https://maruxan.github.io/html-css-practice/card-2/',
+  },
+  {
+    name: 'card-3',
+    url: 'https://maruxan.github.io/html-css-practice/card-3/',
+  },
+  {
+    name: 'card-4',
+    url: 'https://maruxan.github.io/html-css-practice/card-4/',
+  },
+  {
+    name: 'card-5',
+    url: 'https://maruxan.github.io/html-css-practice/card-5/',
+  },
+  {
+    name: 'card-6',
+    url: 'https://maruxan.github.io/html-css-practice/card-6/',
+  },
+  {
+    name: 'card-7',
+    url: 'https://maruxan.github.io/html-css-practice/card-7/',
+  },
+  {
+    name: 'card-8',
+    url: 'https://maruxan.github.io/html-css-practice/card-8/',
+  },
+  {
+    name: 'card-9',
+    url: 'https://maruxan.github.io/html-css-practice/card-9/',
+  },
+  {
+    name: 'landing-1',
+    url: 'https://maruxan.github.io/html-css-practice/landing-1/',
+  },
+  {
+    name: 'landing-2',
+    url: 'https://maruxan.github.io/html-css-practice/landing-2/',
+  },
+  {
+    name: 'landing-3',
+    url: 'https://maruxan.github.io/html-css-practice/landing-3/',
+  },
+];
+
+export const javascript = [
+  {
+    name: 'landing-2010',
+    website: 'https://upbeat-ride-256506.netlify.app/',
+    repo: 'https://github.com/BereReddig/Landing_20-10',
+  },
+  {
+    name: 'the-planets',
+    website: 'https://sun-team.netlify.app/',
+    repo: 'https://github.com/jfiora/the-planets',
+  },
+  {
+    name: 'jeremy-project',
+    website: 'https://jeremy-ttd.netlify.app/',
+    repo: 'https://github.com/monsterkosh/jeremy-project',
+  },
+  {
+    name: 'arch-react-landing',
+    website: 'https://arch-architecture-studio.netlify.app/',
+    repo: 'https://github.com/maruxan/arch-react-landing',
+  },
+];
